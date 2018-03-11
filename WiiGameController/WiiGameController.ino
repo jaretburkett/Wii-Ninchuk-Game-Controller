8c96bded79d84b1e7bab3f54383fbaa9647ec836
Jaret Burkett <jaretburkett@gmail.com>
2018-03-11 13:37:40 -0500

    Added averaging and fixed running

--- WiiGameController/WiiGameController.ino
+++ WiiGameController/WiiGameController.ino
@@ -1,7 +1,13 @@
+// Main Defines
+#define AVG_READINGS 20     // the number of readings to average
+#define POLLING_DELAY 100   // the number of ms to wait between polls
+
+// includes
 #include <Keyboard.h>
 #include <Wire.h>
-#define NUNCHUK_DEBUG true;
 #include "nunchuk.h"
+#include "Average.h"
+
 
 bool isCPressed = false;
 bool isZPressed = false;
@@ -15,20 +21,39 @@ bool isDownPressed = false;
 
 bool isLeanRightPressed = false;
 bool isLeanLeftPressed = false;
-//juml;
+//jump;
 bool isJumpPressed = false;
 int16_t previousAcclZ = 0;
 int16_t previousAcclY = 0;
 int16_t previousAcclX = 0;
-void setup() {
 
+
+// Reserve space for entries in the average bucket.
+// Change the type between < and > to change the entire way the library works.
+Average<int16_t> avgAcclZ(AVG_READINGS);
+bool shouldAverageZ = true;
+Average<int16_t> avgAcclY(AVG_READINGS);
+bool shouldAverageY = true;
+Average<int16_t> avgAcclX(AVG_READINGS);
+bool shouldAverageX = true;
+
+void setup() {
     Serial.begin(9600);
     Keyboard.begin();
     Wire.begin();
     // nunchuk_init_power(); // A1 and A2 is power supply
     nunchuk_init();
     // wait for it to init 
-    delay(3000);
+    while(!nunchuk_read()){
+      delay(100);
+    }
+    // get averages
+    for(uint16_t i = 0; i <= AVG_READINGS*2; i++ ){
+      avgAcclX.push(nunchuk_accelX());
+      avgAcclY.push(nunchuk_accelY());
+      avgAcclZ.push(nunchuk_accelZ());
+      delay(POLLING_DELAY);
+    }
     // get previous
     previousAcclZ = nunchuk_accelX();
     previousAcclY = nunchuk_accelY();
@@ -36,13 +61,16 @@ void setup() {
 }
 
 void checkJump(){
-  if(nunchuk_accelY() < -50 && !isJumpPressed){
+  int16_t breakpoint = (int16_t) avgAcclY.mean() - 50;
+  if(nunchuk_accelY() < breakpoint && !isJumpPressed){
     // first press
     Keyboard.press(' ');
+    shouldAverageY = false;
     isJumpPressed = true;
-  } else if(nunchuk_accelY() >= -50 && isJumpPressed){
+  } else if(nunchuk_accelY() >= breakpoint && isJumpPressed){
     // first release
     Keyboard.release(' ');
+    shouldAverageY = true;
     isJumpPressed = false;
   }
 }
@@ -89,9 +117,19 @@ void checkRun(){
     // press left
     Keyboard.press(KEY_LEFT_SHIFT);
     isShiftPressed = true;
+    if(isUpPressed){
+        // need to repress, catch on next cycle
+        Keyboard.release('w');
+        isUpPressed = false;
+    }
   } else if(nunchuk_joystickY() <= 120 && isShiftPressed){
     Keyboard.release(KEY_LEFT_SHIFT);
     isShiftPressed = false;
+    if(isUpPressed){
+        // need to repress, catch on next cycle
+        Keyboard.release('w');
+        isUpPressed = false;
+    }
   }
 }
 
@@ -151,21 +189,51 @@ void checkJoystick(){
   }
 }
 
+void printData(){
+  Serial.print("joy: X:");
+  Serial.print(nunchuk_joystickX(), DEC);
+  Serial.print(", Y:");
+  Serial.print(nunchuk_joystickY(), DEC);
+
+  Serial.print("  acc: X:");
+  Serial.print(nunchuk_accelX(), DEC);
+  Serial.print(", Xavg:");
+  Serial.print((int16_t) avgAcclX.mean(), DEC);
+  Serial.print(", Y:");
+  Serial.print(nunchuk_accelY(), DEC);
+  Serial.print(", Yavg:");
+  Serial.print((int16_t) avgAcclY.mean(), DEC);
+  Serial.print(", Z:");
+  Serial.print(nunchuk_accelZ(), DEC);
+  Serial.print(", Zavg:");
+  Serial.print((int16_t) avgAcclZ.mean(), DEC);
+
+  Serial.print("  but: Z:");
+  Serial.print(nunchuk_buttonZ(), DEC);
+  Serial.print(", C:");
+  Serial.print(nunchuk_buttonC(), DEC);
+  Serial.print("\n");
+}
+
 void loop() {
     if (nunchuk_read()) {
         // Work with nunchuk_data
-        nunchuk_print();
+        printData();
         checkButtonC();
         checkButtonZ();
         checkJoystick();
         checkJump();
         checkLean();
         checkRun();
+        // push averages
+        if(shouldAverageX) avgAcclX.push(nunchuk_accelX());
+        if(shouldAverageY) avgAcclY.push(nunchuk_accelY());
+        if(shouldAverageZ) avgAcclZ.push(nunchuk_accelZ());
         // set previous
         previousAcclZ = nunchuk_accelX();
         previousAcclY = nunchuk_accelY();
         previousAcclX = nunchuk_accelZ();
     } 
-    delay(100);
+    delay(POLLING_DELAY);
 }
 //
